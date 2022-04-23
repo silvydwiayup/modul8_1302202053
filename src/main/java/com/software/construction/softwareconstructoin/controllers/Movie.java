6bdc1b85b8cb411f8bf8bb81698c0b3ba75e4350
silvydwiayup <silvydwiap88@gmail.com>
2022-04-23 15:06:40 +0700

    commit

---
+++ src/main/java/com/software/construction/softwareconstructoin/controllers/Movie.java
@@ -0,0 +1,2 @@
+package com.software.construction.softwareconstructoin.controllers;public class Movie {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.modul8_1302200103 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.modul8_1302200103 to javafx.fxml;
+    exports com.example.modul8_1302200103;
+}
